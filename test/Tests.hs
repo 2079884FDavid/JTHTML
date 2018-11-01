af393a1057fbc0896f261725f725e33d880c9b13
David Frohlingsdorf <2079884f@student.gla.ac.uk>
2018-11-01 17:58:12 +0100

    Added tests for JTHTML.Tables

--- test/Tests.hs
+++ test/Tests.hs
@@ -9,6 +9,7 @@ import Data.List
 
 import JTHTML.Tags
 import JTHTML.Lists
+import JTHTML.Tables
 
 main = defaultMain tests
 
@@ -22,7 +23,12 @@ tests = [
   testGroup "JTHTML.Lists toUnorderedList" [
     testProperty "Check that list style is correct" prop_jthtmlLists_toUnorderedList_liststyle,
     testCase "Empty list" test_jthtmlLists_toUnorderedList_empty,
-    testCase "Singleton list" test_jthtmlLists_toUnorderedList_oneelement]
+    testCase "Singleton list" test_jthtmlLists_toUnorderedList_oneelement],
+  testGroup "JTHTML.Tables toTable" [
+    testCase "Empty table" test_jthtmlTables_toTable_empty,
+    testCase "No headers" test_jthtmlTables_toTable_noheaders,
+    testCase "Simple table" test_jthtmlTables_toTable_simple,
+    testCase "Different row sizes" test_jthtmlTables_toTable_differentrows]
   ]
 
 prop_jthtmlTags_tagItem_anytag s t = 
@@ -53,3 +59,28 @@ test_jthtmlLists_toUnorderedList_empty =
 
 test_jthtmlLists_toUnorderedList_oneelement =
   toUnorderedList Nothing ["foo"] @?= "<ul><li>foo</li>\n</ul>\n"
+
+test_jthtmlTables_toTable_empty =
+  toTable [] [] @?= "<table style=\"width:100%\"></table>\n"
+
+test_jthtmlTables_toTable_noheaders =
+  toTable [] [["foo", "bar"]] @?= s
+  where
+    s = "<table style=\"width:100%\"><tr><td>foo</td>\n<td>bar</td>\n</tr>\n</table>\n"
+
+test_jthtmlTables_toTable_simple =
+  toTable h c @?= "<table style=\"width:100%\">"++r1++r2++r3++r4++"</table>\n"
+  where
+    h = ["Name", "Nickname"]
+    c = [["Alexander", "Alex"], ["Elizabeth", "Lizzy"], ["Patrick", "Pat"]]
+    r1 = "<tr><th>Name</th>\n<th>Nickname</th>\n</tr>\n"
+    r2 = "<tr><td>Alexander</td>\n<td>Alex</td>\n</tr>\n"
+    r3 = "<tr><td>Elizabeth</td>\n<td>Lizzy</td>\n</tr>\n"
+    r4 = "<tr><td>Patrick</td>\n<td>Pat</td>\n</tr>\n"
+
+test_jthtmlTables_toTable_differentrows =
+  toTable [] c @?= "<table style=\"width:100%\">"++r1++r2++"</table>\n"
+  where
+    c = [["foo", "bar"], ["a", "b", "c"]]
+    r1 = "<tr><td>foo</td>\n<td>bar</td>\n</tr>\n"
+    r2 = "<tr><td>a</td>\n<td>b</td>\n<td>c</td>\n</tr>\n"
